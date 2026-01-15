d75c9581d61afbfb6275bb742ba0cf4684bc5cc4
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-01-16 05:20:11 +0800

    Upload sandwish.py

---
+++ python-files/1768512010806-2ksw-sandwish.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Black Window")
+
+# Set window size
+window.geometry("800x600")
+
+# Set background color to black
+window.configure(bg="black")
+
+window.mainloop()
