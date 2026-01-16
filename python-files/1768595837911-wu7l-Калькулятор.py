28c6f772dd5c9256508844c8bf590ec065fac5dd
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-01-17 04:37:18 +0800

    Upload Калькулятор.py

---
+++ python-files/1768595837911-wu7l-Калькулятор.py
@@ -0,0 +1,18 @@
+rim = int(input("Ввидите число:"))
+
+ram = int(input("Ввидите число:"))
+
+print("Готово <3")
+
+print("Сумма = ")
+
+print(ram + rim)
+
+print("Вычитание = ")
+
+print(ram - rim)
+
+print("Умножение = ")
+
+print(ram * rim)
+
