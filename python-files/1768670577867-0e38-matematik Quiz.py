6bc2f5c135b5be06aed4cb9bbb06c7521c43db95
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-01-18 01:22:58 +0800

    Upload matematik Quiz.py

---
+++ python-files/1768670577867-0e38-matematik Quiz.py
@@ -0,0 +1,59 @@
+bir = int(input("43 + 56 = "))
+if bir == 99:
+    print("doğru")
+else:
+    print("yanlış")
+
+iki = int(input("65 + 78 = "))
+if iki == 143:
+    print("doğru")
+else:
+    print("yanlış")
+
+üç = int(input("52 + 98 = "))
+if üç == 150:
+    print("doğru")
+else:
+    print("yanlış")
+
+dört = int(input("3 x 8 = "))
+if dört == 24:
+    print("doğru")
+else:
+    print("yanlış")
+
+beş = int(input("7 x 11 = "))
+if beş == 77:
+    print("doğru")
+else:
+    print("yanlış")
+
+altı = int(input("9 x 30 = "))
+if altı == 270:
+    print("doğru")
+else:
+    print("yanlış")
+
+yedi = int(input("5 x 51 = "))
+if yedi == 255:
+    print("doğru")
+else:
+    print("yanlış")
+
+sekiz = int(input("67 - 31 = "))
+if sekiz == 36:
+    print("doğru")
+else:
+    print("yanlış")
+
+dokuz = int(input("89 - 76 = "))
+if dokuz == 13:
+    print("doğru")
+else:
+    print("yanlış")
+
+on = int(input("65 x 65 = "))
+if on == 4225:   
+    print("doğru")
+else:
+    print("yanlış")
