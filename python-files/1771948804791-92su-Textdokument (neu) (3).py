f697dd0c50e1f25b84c15d81ca6a5d00a9a7a3b1
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-02-25 00:00:05 +0800

    Upload Textdokument (neu) (3).py

---
+++ python-files/1771948804791-92su-Textdokument (neu) (3).py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Blaues Fenster")
+root.geometry("400x300")  # Breite x Höhe
+root.configure(bg="blue")  # Hintergrundfarbe Blau
+
+root.mainloop()
