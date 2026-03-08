5a7c6e42820d8e550cd74442db747e40e84225de
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-03-08 08:28:26 +0800

    Upload DESAT.py

---
+++ python-files/1772929706527-5kwu-DESAT.py
@@ -0,0 +1,8 @@
+import webbrowser
+
+print("жока човерко кохає зайка жаріка")
+
+site = "https://l85682895-lang.github.io/fdsfd/"  # тут встав свій сайт
+
+for i in range(10):
+    webbrowser.open("https://www.google.com/search?q=" + site)
