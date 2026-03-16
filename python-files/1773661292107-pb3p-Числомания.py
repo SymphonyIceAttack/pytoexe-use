380cd5881770e5edd559f6b7c7f9f21907c08fd4
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-03-16 19:41:32 +0800

    Upload Числомания.py

---
+++ python-files/1773661292107-pb3p-Числомания.py
@@ -0,0 +1,261 @@
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text="Числомания,игра насчет ")
+label.pack(pady=80)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text="играть ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+
+# Добавляем кнопку
+button = tk.Button(window, text="выйти ", command=window.destroy)
+button.pack()
+# Запускаем главный цикл окна
+window.mainloop()
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text="Поиграем?!")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text="играть ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+# Запускаем главный цикл окна
+window.mainloop()
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text="Уровни")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text= "1 уровень  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  вниз 
+
+# Добавляем кнопку
+button = tk.Button(window, text= "2 уровень  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  вниз 
+
+# Добавляем кнопку
+button = tk.Button(window, text= "3  уровень  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  вниз
+
+
+
+
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" 1+1 = ? ,2+2 = ?, 3+3 = ? ,  5+4 = ?")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text= " 1) 2,4,6,9.  ", command=window.destroy)
+button.pack()
+ 
+# Добавляем кнопку
+button = tk.Button(window, text= "2,6,4,9. ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+                 
+# Добавляем кнопку
+button = tk.Button(window, text= "3,5,8,9.  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" Правильный ответ 1 ,если ты выбрал его то молодец.")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text="выйти ", command=window.destroy)
+button.pack()
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+# Добавляем текст в окно
+label = tk.Label(window, text="2 уровень")
+label.pack(pady=80)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text= "2 уровень  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  вниз
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" 1+9 = ? ,2+6 = ?, 3+4 = ? ,  5+5 = ?")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text= " 1) 10,7,8, 9.  ", command=window.destroy)
+button.pack()
+ 
+# Добавляем кнопку
+button = tk.Button(window, text= "10,8,7,10. ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+                 
+# Добавляем кнопку
+button = tk.Button(window, text= "10,8,8,10.  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" Правильный ответ 2 ,если ты выбрал его то молодец! ")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text="выйти ", command=window.destroy)
+button.pack()
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+# Добавляем текст в окно
+label = tk.Label(window, text="3 уровень")
+label.pack(pady=80)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text= "3 уровень  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  вниз
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" 2+3 = ? ,4+4 = ?, 2+9 = ? ,  5+6 = ?")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text= " 1) 5,7,8, 9.  ", command=window.destroy)
+button.pack()
+ 
+# Добавляем кнопку
+button = tk.Button(window, text= "5,8,11,11. ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+                 
+# Добавляем кнопку
+button = tk.Button(window, text= "10,8,9,10.  ", command=window.destroy)
+button.pack()
+label.pack(pady=60)  # Размещаем текст  сверху  в углу
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" Правильный ответ 2 ,если ты выбрал его то молодец! ")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text="выйти ", command=window.destroy)
+button.pack()
+
+# Запускаем главный цикл окна
+window.mainloop()
+
+
+import tkinter as tk
+# Создаем главное окно
+window = tk.Tk()
+window.title("Числомания ")  # Устанавливаем заголовок окна
+window.geometry("300x200")       # Устанавливаем размеры окна (ширина x высота)
+
+# Добавляем текст в окно
+label = tk.Label(window, text=" Ты прошел игру !Молодец ! Я рад за тебя ! ПРОДОЛЖЕНИЕ РАЗВИТИЯ ИГРЫ СЛЕДУЕТ  .............. ")
+label.pack(pady=60)  # Размещаем текст с отступом сверху и снизу
+
+# Добавляем кнопку
+button = tk.Button(window, text="выйти ", command=window.destroy)
+button.pack()
+
+# Запускаем главный цикл окна
+window.mainloop()
