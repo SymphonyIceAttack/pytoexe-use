537b38b2815ed278834a95300f7084e342ba3603
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-03-20 23:00:44 +0800

    Upload CristalOS.py

---
+++ python-files/1774018844656-fynt-CristalOS.py
@@ -0,0 +1,17 @@
+import time
+import playsound
+from playsound import playsound
+
+# for playing note.mp3 file
+playsound('/path/note.mp3')
+print('playing sound using  playsound')
+
+
+print ("Your OS starting, please wait...")
+time.sleep (3)
+print ("loading... Hah")
+time.sleep (1)
+print ("VIRTUAL OS CRISTAL OS")
+# for playing note.mp3 file
+playsound('QWERTY START (1).mp3')
+print('playing sound using  playsound')
