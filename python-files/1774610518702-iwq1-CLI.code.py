4d9191c3a8a408735fedf19e4d7d3858d3747120
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-03-27 19:21:58 +0800

    Upload CLI.code.py

---
+++ python-files/1774610518702-iwq1-CLI.code.py
@@ -0,0 +1,98 @@
+import sys
+import random
+import time
+print("CLI.code/0.5.1/beta\n")
+
+# Чем гуще лес if else if else
+
+while True:
+    text = input("Write code: ")
+
+    if text == "cli.code.lov=":
+        print("\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3")
+
+#Коды
+    elif text == "cli.secret.code=":
+        print("卐")
+
+    elif text == "cli.lol.coding=":
+        print("( ͡° ͜ʖ ͡°)")
+
+    elif text == "cli.lol2.coding=":
+        print("(•‿•)")
+
+    elif text == "cli.lol3.coding=":
+        print("ಠ_ಠ")
+
+    elif text == "cli.lol4.code=":
+        print("ʕ•ᴥ•ʔ")
+    
+    elif text == "cli.lol5.code=":
+        print("0_0")
+
+    elif text == "cli.lol6.code=":
+        print("  ඞ  ")
+
+    elif text == "cli.lol7.code=":
+        print(r"""    ______
+   / ____ \
+  | | 👁  | |
+  | |____| |
+  |  ____  |
+  | |    | |
+  | |    | |
+  | |____| |
+   \______/
+    /|  |\
+   /_|  |_\
+   IMPOSTOR
+""")
+
+    elif text == "cli.art1=":
+        print("(•_•)\n( •_•)>⌐■-■\n(⌐■_■)")
+
+    elif text == "cli.calculator.activate=":
+        print(eval(input("Enter a math problem: ")))
+
+    elif text == "cli.program.characteristics=":
+        print("")
+        print("Version: 0.5.1 Beta")
+        print("Name: cli.code")
+        print("Developer: FXRMA\n")
+    
+    elif text == "cli.best=":
+        print("You Best \3")
+
+    elif text == "cli.art2=":
+        print(""" /\_/\  
+( o.o ) 
+ > ^ <
+""")
+
+    elif text == "cli.art3=":
+        print(r"""   /\
+  /  \
+ |    |
+ |NASA|
+ |    |
+/|____|\
+  /||\
+ /_||_\
+""")
+
+    elif text == "cli.art4=":
+        print(r"""   .-.
+  (o o)
+  | O \
+  |   |
+ /|   |\
+/_|   |_\
+""")
+
+    elif text == "cli.close.code=":
+        print("Closing CLI...")
+        time.sleep(3)
+        sys.exit()
+
+    else:
+        print("Error")
