d0c3ff8053376eead2e0bf66cfc37f2a616dfb3f
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-04-02 18:24:27 +0800

    Upload main.py

---
+++ python-files/1775125466627-lv6o-main.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+def main():
+    name = input("Enter your name: ")
+    if name:
+        webbrowser.open("https://www.youtube.com/watch?v=r11mz5jvHaU")
+
+if __name__ == "__main__":
+    main()
