faa5539cccb866707958e399fb516c6d578cf1ac
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-04-02 18:20:36 +0800

    Upload main.py

---
+++ python-files/1775125235898-ilfx-main.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+def main():
+    name = input("Enter your name: ")
+    if name:
+        webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+if __name__ == "__main__":
+    main()
