5ea0b7537cdfbb1af344032aea7a549867121374
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-04-02 20:14:27 +0800

    Upload gift.py

---
+++ python-files/1775132066686-0mxf-gift.py
@@ -0,0 +1,9 @@
+import webbrowser
+
+def main():
+    name = input("Enter your name: ")
+    if name:
+        webbrowser.open("https://www.youtube.com/watch?v=fC7oUOUEEi4")
+
+if __name__ == "__main__":
+    main()
