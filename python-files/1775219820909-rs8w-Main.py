4d3d64a008daf16bd770512ceeebd6d654379994
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-04-03 20:37:01 +0800

    Upload Main.py

---
+++ python-files/1775219820909-rs8w-Main.py
@@ -0,0 +1,26 @@
+def time():
+    import time
+    t=time.localtime(time.time())
+    locatime=time.asctime(t)
+    settime='Now:'+time.asctime(t)
+    print(settime)
+    time.sleep(1)
+
+
+while True:
+    while True:
+        while True:
+            while True:
+                while True:
+                    while True:
+                        while True:
+                            while True:
+                                while True:
+                                    while True:
+                                        while True:
+                                            while True:
+                                                while True:
+                                                    while True:
+                                                        while True:
+                                                            while True:
+                                                                time()
