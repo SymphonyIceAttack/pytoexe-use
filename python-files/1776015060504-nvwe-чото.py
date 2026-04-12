20140867b7660b4ecf47c825cc54bed9bd0ec540
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-04-13 01:31:00 +0800

    Upload чото.py

---
+++ python-files/1776015060504-nvwe-чото.py
@@ -0,0 +1,34 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Слава гитлеру")
+root.geometry("250x1000+820+50")
+root.configure(bg='black')  
+
+root1 = tk.Tk()
+root1.title("Слава Гитлеру")
+root1.geometry("1000x250+410+400")
+root1.configure(bg='black') 
+
+root3 = tk.Tk()
+root3.title("Слава Гитлеру")
+root3.geometry("600x250+820+50")
+root3.configure(bg='black') 
+
+root4 = tk.Tk()
+root4.title("Слава Гитлеру")
+root4.geometry("600x250+410+800")
+root4.configure(bg='black') 
+
+root5 = tk.Tk()
+root5.title("Слава Гитлеру")
+root5.geometry("250x600+410+50")
+root5.configure(bg='black') 
+
+root6 = tk.Tk()
+root6.title("Слава Гитлеру")
+root6.geometry("250x600+1160+400")
+root6.configure(bg='black') 
+
+
+
