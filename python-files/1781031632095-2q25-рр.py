b4412d6a7404322ae2ee07b2d47a705a88dd239d
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-06-10 03:00:32 +0800

    Upload рр.py

---
+++ python-files/1781031632095-2q25-рр.py
@@ -0,0 +1,7 @@
+import random
+
+import random
+
+fruits = ["https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1qZ_9SYKoSXEAXi71BCUy5E2D4kFmgD5T  Скачайте и вставьте", "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=132sPE7BUAjEonC5Myi1pr9D5S0oMmEq0  Скачайте и вставьте", "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1IhnnN0CceOqTAboKNGkW2KtIsXYzNb8o  Скачайте и вставьте"]
+random_fruit = random.choice(fruits)
+print(random_fruit)
