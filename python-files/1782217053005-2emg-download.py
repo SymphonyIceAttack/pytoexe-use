3252d469c07a46f8c3aab06294e8f61afe417d1c
SymphonyIceAttack <79434860+SymphonyIceAttack@users.noreply.github.com>
2026-06-23 20:17:33 +0800

    Upload download.py

---
+++ python-files/1782217053005-2emg-download.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import messagebox, simpledialog
+import time
+import random
+
+class TypingTest:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Тест скорости печати")
+        
+        
